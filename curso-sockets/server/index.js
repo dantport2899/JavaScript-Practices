ff03c994070ab97b266147ab18847c04ccb36fe6
Dante Portales <dantport28@gmail.com>
2022-03-22 18:33:22 -0600

    class 21

---
+++ curso-sockets/server/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+const { measureMemory } = require('vm');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server,{
+    cors: { // Permite el acceso de orígenes mixtos (CORS)
+        origin: '*'
+    }
+});
+
+app.use(express.static('client'));
+
+app.get('/hola-mundo',function(req,res){
+    res.status(200).send("Hola mundo desde una ruta");
+});
+
+var messages = [{
+    id:1,
+    text:'Bienvenido al chat privado de Shocket.io y NodeJS...',
+    nickname: 'Bot - Dante Portales'
+}];
+
+io.on('connection', function(socket){
+    console.log("El nodo con IP:"+socket.handshake.address+" se ha conectado...");
+    socket.emit('messages', messages);
+
+    socket.on('add-message', function(data){
+        messages.push(data);
+        io.sockets.emit('messages', messages);
+    });
+});
+
+server.listen(6677, function(){
+    console.log('Servidor esta funcionando en http://localhost:6677');
+});
